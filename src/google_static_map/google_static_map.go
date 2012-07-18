902c637bab32155c4eb4f4c553925954c6618c59
Eric <eric@example.com>
2012-07-17 23:24:24 -0400

    Fill out functionality.

--- src/google_static_map/google_static_map.go
+++ src/google_static_map/google_static_map.go
@@ -2,13 +2,96 @@ package google_static_map
 
 import (
 	"bytes"
+	"fmt"
+	"net/url"
 )
+
+type Location interface {
+	GetLocation() string
+}
 
 type Point struct {
 	lat, lon float64
 }
 
-type PolyLine     []Point
+type Address string
+
+type PolyLine struct {
+	locations         []Location
+	allPointLocations bool
+	ClosePath         bool
+	Weight            *int
+	Color             *string
+	FillColor         *string
+}
+
+const URL_HEAD = "http://maps.googleapis.com/maps/api/staticmap?"
+
+type Map struct {
+	sensor        bool
+	markers       []Location
+	paths         []*PolyLine
+	width, height int
+	scale         *int
+}
+
+var precedeChar map[bool]string
+
+func init() {
+	precedeChar = make(map[bool]string)
+	precedeChar[true] = "="
+	precedeChar[false] = url.QueryEscape("|")
+}
+
+func NewMap(width, height int, scale int) *Map {
+	result := new(Map)
+	result.sensor = false
+	result.width = width
+	result.height = height
+	if scale != 0 {
+		result.scale = &scale
+	}
+	result.markers = make([]Location, 0, 10)
+	result.paths = make([]*PolyLine, 0, 10)
+	return result
+}
+
+func (m *Map) AddMarker(l Location) {
+	m.markers = append(m.markers, l)
+}
+
+func (m *Map) AddPath(p *PolyLine) {
+	m.paths = append(m.paths, p)
+}
+
+func (m *Map) Encode() string {
+	buffer := new(bytes.Buffer)
+
+	buffer.WriteString(URL_HEAD)
+	buffer.WriteString(fmt.Sprint("sensor=", m.sensor))
+	buffer.WriteString(fmt.Sprint("&size=", m.width, "x", m.height))
+	if m.scale != nil {
+		buffer.WriteString(fmt.Sprint("&scale=", *m.scale))
+	}
+
+	for _, path := range m.paths {
+		buffer.WriteString("&")
+		buffer.WriteString(path.Encode())
+	}
+
+	if len(m.markers) > 0 {
+		buffer.WriteString("&markers")
+		firstParam := true
+
+		for _, marker := range m.markers {
+			buffer.WriteString(precedeChar[firstParam])
+			firstParam = false
+			buffer.WriteString(marker.GetLocation())
+		}
+	}
+
+	return buffer.String()
+}
 
 func round(in float64) int32 {
 	if in >= 0 {
@@ -19,13 +102,31 @@ func round(in float64) int32 {
 	return int32(in)
 }
 
-func NewPolyLine() *PolyLine {
-	result := PolyLine(make([]Point, 0, 10))
-	return &result
+func NewPoint(lat, lon float64) Point {
+	return Point{lat, lon}
 }
 
-func (pl *PolyLine) AddPoint(pt Point) {
-	*pl = append(*pl, pt)
+func (p Point) GetLocation() string {
+	return fmt.Sprintf("%0.5f,%0.5f", p.lat, p.lon)
+}
+
+func (a Address) GetLocation() string {
+	return string(a)
+}
+
+func NewPolyLine() *PolyLine {
+	result := new(PolyLine)
+	result.ClosePath = false
+	result.locations = make([]Location, 0, 10)
+	result.allPointLocations = true
+	return result
+}
+
+func (pl *PolyLine) AddPoint(l Location) {
+	pl.locations = append(pl.locations, l)
+	if _, isPoint := l.(Point); !isPoint {
+		pl.allPointLocations = false
+	}
 }
 
 func (pl *PolyLine) AddPointLatLon(lat, lon float64) {
@@ -37,7 +138,7 @@ func (pl *PolyLine) AddPointLatLon(lat, lon float64) {
  * https://developers.google.com/maps/documentation/utilities/polylinealgorithm
  */
 func EncodeSignedFloat(v float64) string {
-	i := round(v * 100000) << 1
+	i := round(v*100000) << 1
 	if i < 0 {
 		i = -i - 1
 	}
@@ -71,19 +172,63 @@ func EncodeUnsignedInt(v uint32) string {
 	return buffer.String()
 }
 
-func (pl *PolyLine) Encode(closeLoop bool) string {
+func (pl *PolyLine) EncodeLocations() string {
 	buffer := new(bytes.Buffer)
 	prevLat, prevLon := 0.0, 0.0
-	for _, p := range []Point(*pl) {
-		deltaLat, deltaLon := p.lat - prevLat, p.lon - prevLon
-		prevLat, prevLon = p.lat, p.lon
+	for _, loc := range pl.locations {
+		pt := loc.(Point)
+		deltaLat, deltaLon := pt.lat-prevLat, pt.lon-prevLon
+		prevLat, prevLon = pt.lat, pt.lon
 		buffer.WriteString(EncodeSignedFloat(deltaLat))
 		buffer.WriteString(EncodeSignedFloat(deltaLon))
 	}
-	if closeLoop {
-		deltaLat, deltaLon := (*pl)[0].lat - prevLat, (*pl)[0].lon - prevLon
+	if pl.ClosePath {
+		pt := pl.locations[0].(Point)
+		deltaLat, deltaLon := pt.lat-prevLat, pt.lon-prevLon
 		buffer.WriteString(EncodeSignedFloat(deltaLat))
 		buffer.WriteString(EncodeSignedFloat(deltaLon))
 	}
+	return buffer.String()
+}
+
+func (pl *PolyLine) Encode() string {
+	buffer := new(bytes.Buffer)
+	buffer.WriteString("path")
+	firstParam := true
+
+	if pl.Weight != nil {
+		buffer.WriteString(precedeChar[firstParam])
+		firstParam = false
+		buffer.WriteString("weight:")
+		buffer.WriteString(fmt.Sprint(*pl.Weight))
+	}
+
+	if pl.Color != nil {
+		buffer.WriteString(precedeChar[firstParam])
+		firstParam = false
+		buffer.WriteString("color:")
+		buffer.WriteString(*pl.Color)
+	}
+
+	if pl.FillColor != nil {
+		buffer.WriteString(precedeChar[firstParam])
+		firstParam = false
+		buffer.WriteString("fillcolor:")
+		buffer.WriteString(*pl.FillColor)
+	}
+
+	if pl.allPointLocations {
+		buffer.WriteString(precedeChar[firstParam])
+		firstParam = false
+		buffer.WriteString("enc:")
+		buffer.WriteString(url.QueryEscape(pl.EncodeLocations()))
+	} else {
+		for _, loc := range pl.locations {
+			buffer.WriteString(precedeChar[firstParam])
+			firstParam = false
+			buffer.WriteString(url.QueryEscape(loc.GetLocation()))
+		}
+	}
+
 	return buffer.String()
 }
